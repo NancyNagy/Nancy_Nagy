7c2c08c76736983a666743e2ccd8f7ecb436bc8e
NancyNagy <nnagy686@gmail.com>
2022-04-29 12:33:30 +0200

    Task_6

---
+++ Task_6/Dashboard2/app/Http/Controllers/Dashboard/ProductController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Http\Controllers\Dashboard;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+
+class ProductController extends Controller
+{
+    public function index()
+    {
+        $products = DB::table('products')->get();
+        return view('products.index', compact('products'));
+    }
+}
